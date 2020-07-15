f069d4dff8c0a8413f30925bdb1847af3fcfb38f
muhdarifrawi <muhdarifrawi@gmail.com>
2020-07-15 16:16:12 +0800

    Generated form submission template - able to enter number into input - number is returned back to index template

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main_page():
     return render_template("index.html")
+
+@app.route('/', methods=["POST"])
+def generate_email():
+    number_of_emails = request.form["number"]
+
+    return render_template("index.html", numberOfEmails=number_of_emails)
