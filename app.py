4ad9a9ee0a9f1b2889741c504067fb24aa25729a
muhdarifrawi <muhdarifrawi@gmail.com>
2020-07-15 20:07:51 +0800

    Added email generator - added email gen in app.py - output logic on jinjja allows system to show either list of eamil or a message letting users know to enter appropriate numbers

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,12 +10,41 @@ def main_page():
 @app.route('/', methods=["POST"])
 def generate_email():
     number_of_emails = int(request.form["number"])
+
+
+    animals = ["antelope", "bat", "cat", "dog", "elephant", "fox", "giraffe", "hippopotamus", "impala",
+            "jaguar", "kangaroo", "lemur", "monkey", "narwhal", "ostrich", "panda", "quail", "rooster", "snake",
+            "tortoise",
+            "urchin", "vulture", "whale", "xeme", "yak", "zebra"]
+
+    fruits = ["apple", "banana", "ciku", "durian", "elderberry", "fig", "grape", "huckleberry", "ice apple", "jackfruit",
+            "kiwi", "lime", "melon", "orange", "pumpkin", "quince", "raspberry", "strawberry", "tangerine", "ugli",
+            "voavanga",
+            "watermelon", "yam", "zuchinni"]
+
+    animal = random.choice(animals)
+    fruit = random.choice(fruits)
+    email = str(animal) + str(fruit) + "@asd.com"
+
+    counter = 0
+    email_collection = []
+    
+
+    # print(email_collection)
+    # print(len(email_collection))
+
     
     if (number_of_emails>0 and number_of_emails < 1000):
-        print("here", number_of_emails)
+        
+        while counter < number_of_emails:
+            animal = random.choice(animals)
+            fruit = random.choice(fruits)
+            email = str(animal) + str(fruit) + "@asd.com"
+            email_collection.append(email)
+            counter += 1
 
-        return render_template("index.html", numberOfEmails=number_of_emails)
+        return render_template("index.html", numberOfEmails=email_collection)
     
     else:
-        print("there", number_of_emails)
+        
         return render_template("index.html", numberOfEmails="Enter a number between 1 to 999.")
