64967163369e5e3b4fbdfa1fbb7ebfe9e1c2bd34
muhdarifrawi <muhdarifrawi@gmail.com>
2020-07-15 17:50:55 +0800

    Added logic for python side - Made form input into number to prevent anything other than number inserted - Flask will redirect if number is above than 999 and lesser than 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +7,13 @@ def main_page():
 
 @app.route('/', methods=["POST"])
 def generate_email():
-    number_of_emails = request.form["number"]
+    number_of_emails = int(request.form["number"])
+    
+    if (number_of_emails>0 and number_of_emails < 1000):
+        print("here", number_of_emails)
 
-    return render_template("index.html", numberOfEmails=number_of_emails)
+        return render_template("index.html", numberOfEmails=number_of_emails)
+    
+    else:
+        print("there", number_of_emails)
+        return render_template("index.html", numberOfEmails="Enter a number between 1 to 999.")
